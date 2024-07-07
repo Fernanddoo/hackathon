1f6ef49961ae64091fcfdc070ddd9bc434c549ec
Fernando <AWPfernandodradunz@gmail.com>
2024-07-07 17:19:06 -0300

    axios fix & frontend styles

--- hackathon-backend/src/app.ts
+++ hackathon-backend/src/app.ts
@@ -1,9 +1,15 @@
 import express from "express";
 import appRouter from "./routes";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors({
+    origin: "http://localhost:3000", 
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
 app.use("/api", appRouter);
 
 export default app;
--- hackathon-backend/src/routes/index.ts
+++ hackathon-backend/src/routes/index.ts
@@ -1,5 +1,5 @@
 import express  from "express";
-import routerAvaliador from "../routes/avaliadorRoutes";
+import routerAvaliador from "./avaliadorRoutes";
 import routerEquipe from "./equipeRoutes";
 
 const appRouter = express();
